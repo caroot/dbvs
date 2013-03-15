01d13d8a13bd6f2d925e5454bd14859da8416afa
MoodB <tiny6de@yahoo.de>
2013-03-15 12:42:52 +0100

    Getter and setter methods

--- src/main/java/org/jboss/tools/examples/model/PhotoAlbum.java
+++ src/main/java/org/jboss/tools/examples/model/PhotoAlbum.java
@@ -16,7 +16,7 @@ public class PhotoAlbum {
 	private String name = "";
 	private String beschreibung = "";
 	private int anzahl = 0;
-	private int dateigroese =0;
+	private int dateigroesse =0;
 	private int id;
 	
 	private HashMap<Integer, PhotoAlbumEntry> hashPhotoAlbumEntry;
@@ -48,6 +48,82 @@ public class PhotoAlbum {
 	 */
 	public void setName(String name) throws IllegalArgumentException {
 		this.name = name;
+	}
+	
+	/**
+	 * Get the description of the photo album
+	 * 
+	 * @return
+	 */
+    public String getBeschreibung() {
+		return beschreibung;
+	}
+    
+    /**
+	 * Sets the description of the photo album.
+	 * 
+	 * @param name
+	 * @throws IllegalArgumentException
+	 */
+     public void setBeschreibung(String beschreibung) throws IllegalArgumentException {
+		this.beschreibung = beschreibung;
+	}
+	
+	/**
+	 * Get the number of the photo album
+	 * 
+	 * @return
+	 */
+     public int getAnzahl() {
+		return anzahl;
+	}
+     
+     /**
+ 	 * Sets the number of the photo album.
+ 	 * 
+ 	 * @param name
+ 	 * @throws IllegalArgumentException
+ 	 */
+     public void setAnzahl(int anzahl) throws IllegalArgumentException {
+		this.anzahl = anzahl;
+	}
+	
+	/**
+	 * Get the size of the photo album
+	 * 
+	 * @return
+	 */
+    public int getDateigroesse() {
+		return dateigroesse;
+	}
+    
+    /**
+	 * Sets the size of the photo album.
+	 * 
+	 * @param name
+	 * @throws IllegalArgumentException
+	 */
+    public void setDateigroese(int dateigroesse) throws IllegalArgumentException {
+		this.dateigroesse = dateigroesse;
+	}
+	
+	/**
+	 * Get the id of the photo album
+	 * 
+	 * @return
+	 */
+    public int getId() {
+		return id;
+	}
+	
+	/**
+	 * Sets the id of the photo album.
+	 * 
+	 * @param name
+	 * @throws IllegalArgumentException
+	 */
+    public void setId(int id) throws IllegalArgumentException {
+		this.id = id;
 	}
 
 	/*
